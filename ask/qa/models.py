0eddda936b35dbaea8d3b3f50f1467d4e621c016
Mishka <mmishkin747@gmail.com>
2021-08-02 16:51:36 +0300

    step23 fixed qa.models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -11,17 +11,19 @@ class QuestionManage(models.Manager):
     def popular(self):
         return self.order_by('-rating')
 
- class Question(models.Model):
-     objects = QuestionManage()
-     title = models.CharField(max_length=255)
-     text = models.TextField()
-     added_at = models.DateTimeField(blank=True, auto_now_add=True)
-     rating = models.IntegerField(blank=True, null=True)
-     author = models.ForeignKey(User, on_delete=models.CASCADE)
-     likes = models.ManyToManyField(User, related_name='likes')
+
+class Question(models.Model):
+    objects = QuestionManage()
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True, auto_now_add=True)
+    rating = models.IntegerField(blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='likes')
+
 
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(blank=True, auto_now_add=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    author = models.ForeingKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
