8c4fab78770563c2f7a9894d28f3b25d5a7b3ee4
mmishkin747 <mmishkin747@gmail.com>
2021-08-13 11:44:15 +0300

    add forms, step 2.6 version 001

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -6,12 +6,13 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^popular/.*', views.popular, name='popular'),
-    url(r'^ask/.*', views.test, name='ask'),
-    url(r'^answer/.*', views.test, name='answer'),
+    url(r'^ask/.*', views.ask, name='ask'),
+    url(r'^answer/.*', views.answer, name='answer'),
     url(r'^signup/.*', views.test, name='signup'),
     url(r'^login/.*$', views.test, name='login'),
     url(r'^logout/.*', views.test, name='logout'),
     url(r'^new/.*$', views.test),
-    url(r'question/(?P<question_id>[0-9]+)/$', views.question, name='question'),
+    url(r'question/(?P<question_id>[0-9]+)/$',
+        views.question, name='question'),
     url('test/', views.test)
 ]
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -3,6 +3,8 @@ from .models import Question, Answer
 from django.views.decorators.http import require_GET
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from .forms import AnswerForm, AskForm
+
 
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
@@ -15,13 +17,13 @@ def index(request):
         page = 1
     except TypeError:
         page = 1
-    questions = Question.objects.new()
+    questions = Question.objects.all().order_by('-id')
     paginator = Paginator(questions, 10)
     page = paginator.page(page)
     context = {
         'title': 'LaTest',
         'paginator': paginator,
-        'questions':page.object_list,
+        'questions': page.object_list,
         'page': page,
         'user': request.user,
         'session': request.session,
@@ -42,18 +44,47 @@ def popular(request):
     context = {
         'title': 'Popular',
         'paginator': paginator,
-        'questions':page.object_list,
+        'questions': page.object_list,
         'page': page,
         'user': request.user,
         'session': request.session,
     }
     return render(request, 'qa/popular.html', context)
 
+
 def question(request, question_id):
     q = get_object_or_404(Question, id=question_id)
-    a = Answer.objects.filter(question=question_id).order_by('-added_at')
+    a = q.answer_set.all()
+    #a = Answer.objects.filter(question=question_id).order_by('-added_at')
+    form = AnswerForm(initial={'question': question_id})
     context = {
-        'question':q,
+        'question': q,
         'answer': a,
+        'form': form,
     }
     return render(request, 'qa/question.html', context)
+
+
+def ask(request):
+    if request.method == 'POST':
+        form = AskForm(request.POST)
+        if form.is_valid():
+            form._user = request.user
+            question = form.save()
+            url = question.get_url()
+            return HttpResponseRedirect(url)
+    else:
+        form = AskForm()
+    return render(request, 'qa/ask.html', {'form': form})
+
+
+def answer(request):
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            print('Answer is valid')
+            form._user = request.user
+            answer = form.save()
+            url = answer.get_url()
+            return HttpResponseRedirect(url)
+    return HttpResponseRedirect('/')
